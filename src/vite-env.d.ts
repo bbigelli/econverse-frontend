1e1ae867462aee49dd89c9b811508ced17a1f6c2
BrunoBigelli <bbigelli@hotmail.com>
2026-04-27 16:33:47 -0300

    fix: vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,3 @@
-// src/vite-env.d.ts
 /// <reference types="vite/client" />
 
 declare module '*.png' {
@@ -28,5 +27,10 @@ declare module '*.gif' {
 
 declare module '*.webp' {
   const value: string
+  export default value
+}
+
+declare module '*.json' {
+  const value: any
   export default value
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
+    
+  assetsInclude: ['**/*.json'],
+  
   css: {
     preprocessorOptions: {
       scss: {
